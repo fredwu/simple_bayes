78188139dc5a4ab25dcf2b69b83bd3f2ff9eaaa2
Fred Wu <ifredwu@gmail.com>
2016-07-16 20:09:37 +1000

    Tweaked configurations

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ A Simple Bayes (a.k.a. [Naive Bayes](https://en.wikipedia.org/wiki/Naive_Bayes_c
 
 - [x] Multinomial Naive Bayes algorithm
 - [x] No external dependencies
-- [x] Ignores English stop words
+- [x] Ignores stop words
 - [x] [Additive smoothing](https://en.wikipedia.org/wiki/Additive_smoothing)
 - [x] [TF-IDF](https://en.wikipedia.org/wiki/Tf-idf)
 - [x] Optional keywords weighting
 
-## API
+## Usage
 
 ```elixir
 bayes = SimpleBayes.init
@@ -34,6 +34,30 @@ bayes |> SimpleBayes.classify("Maybe green maybe red but definitely round and sw
 #   orange: 0.14447781772131096,
 #   banana: 0.10123406763124557
 # ]
+```
+
+### Configuration (Optional)
+
+In your application's `config/config.exs`:
+
+```elixir
+config :simple_bayes, default_weight: 1
+config :simple_bayes, smoothing: 0.001
+config :simple_bayes, stop_words: ~w(
+  a about above after again against all am an and any are aren't as at be
+  because been before being below between both but by can't cannot could
+  couldn't did didn't do does doesn't doing don't down during each few for from
+  further had hadn't has hasn't have haven't having he he'd he'll he's her here
+  here's hers herself him himself his how how's i i'd i'll i'm i've if in into
+  is isn't it it's its itself let's me more most mustn't my myself no nor not of
+  off on once only or other ought our ours ourselves out over own same shan't
+  she she'd she'll she's should shouldn't so some such than that that's the
+  their theirs them themselves then there there's these they they'd they'll
+  they're they've this those through to too under until up very was wasn't we
+  we'd we'll we're we've were weren't what what's when when's where where's
+  which while who who's whom why why's with won't would wouldn't you you'd
+  you'll you're you've your yours yourself yourselves
+)
 ```
 
 ## License
--- lib/simple_bayes.ex
+++ lib/simple_bayes.ex
@@ -1,15 +1,33 @@
 defmodule SimpleBayes do
   defstruct categories: %{}, trainings: 0, tokens: %{}, tokens_per_training: []
 
+  @default_weight 1
+  @smoothing 0.001
+  @stop_words ~w(
+    a about above after again against all am an and any are aren't as at be
+    because been before being below between both but by can't cannot could
+    couldn't did didn't do does doesn't doing don't down during each few for from
+    further had hadn't has hasn't have haven't having he he'd he'll he's her here
+    here's hers herself him himself his how how's i i'd i'll i'm i've if in into
+    is isn't it it's its itself let's me more most mustn't my myself no nor not of
+    off on once only or other ought our ours ourselves out over own same shan't
+    she she'd she'll she's should shouldn't so some such than that that's the
+    their theirs them themselves then there there's these they they'd they'll
+    they're they've this those through to too under until up very was wasn't we
+    we'd we'll we're we've were weren't what what's when when's where where's
+    which while who who's whom why why's with won't would wouldn't you you'd
+    you'll you're you've your yours yourself yourselves
+  )
+
   def init do
     {:ok, agent} = Agent.start_link fn -> %SimpleBayes{} end
 
     agent
   end
 
-  def default_weight, do: Application.get_env(:simple_bayes, :default_weight)
-  def smoothing,      do: Application.get_env(:simple_bayes, :smoothing)
-  def stop_words,     do: Application.get_env(:simple_bayes, :stop_words)
+  def default_weight, do: Application.get_env(:simple_bayes, :default_weight) || @default_weight
+  def smoothing,      do: Application.get_env(:simple_bayes, :smoothing)      || @smoothing
+  def stop_words,     do: Application.get_env(:simple_bayes, :stop_words)     || @stop_words
 
   defdelegate train(agent, category, string, opts \\ []), to: SimpleBayes.Trainer
   defdelegate classify(agent, string),                    to: SimpleBayes.Classifier
