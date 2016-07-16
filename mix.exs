18a8fa380ed8dbb144bec7d1c7c3516f8cfe25bc
Fred Wu <ifredwu@gmail.com>
2016-07-16 20:11:14 +1000

    Bump to v0.3.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SimpleBayes.Mixfile do
 
   def project do
     [app: :simple_bayes,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.3",
      name: "Simple Bayes",
      package: package(),
