d3d1f533f530f3cc8a9e5d85a7cf4b280ba6c750
Fred Wu <ifredwu@gmail.com>
2016-07-28 20:44:22 +1000

    Refactored `SimpleBayes.Classifier.Model.Bernoulli`

--- lib/simple_bayes/classifier/models/bernoulli.ex
+++ lib/simple_bayes/classifier/models/bernoulli.ex
@@ -1,29 +1,46 @@
 defmodule SimpleBayes.Classifier.Model.Bernoulli do
   def probability_of(category, categories_map, data) do
-    cat_trainings    = data.categories[category][:trainings]
-    denominator_base = cat_trainings + 2
+    numerator   = cat_trainings_count(category, data)
+                  * token_count(category, categories_map, data)
 
-    numerator   = cat_trainings * category_numerator(category, denominator_base, categories_map, data)
-    denominator = data.trainings * :math.pow(denominator_base, Kernel.map_size(data.tokens))
+    denominator = data.trainings
+                  * :math.pow(
+                    cat_base_count(category, data),
+                    Kernel.map_size(data.tokens)
+                  )
 
     numerator / denominator
   end
 
-  defp category_numerator(category, denominator_base, categories_map, data) do
+  defp token_count(category, categories_map, data) do
     Enum.reduce(data.tokens, 1, fn ({token, _}, acc) ->
-      token_in_trainings = data.tokens_per_training
-        |> Enum.filter(fn ({{cat, cat_tokens_map}, _}) ->
-          cat == category && Map.has_key?(cat_tokens_map, token)
-        end)
-        |> Enum.count()
+      count = trainings_with_token_count(
+        data.tokens_per_training, category, token
+      ) + 1
 
       value = if Map.has_key?(categories_map, token) do
-        (token_in_trainings + 1)
+        count
       else
-        (denominator_base - (token_in_trainings + 1))
+        cat_base_count(category, data) - count
       end
 
       value * acc
     end)
+  end
+
+  defp trainings_with_token_count(tokens_map, category, token) do
+    tokens_map
+    |> Enum.filter(fn ({{cat, cat_tokens_map}, _}) ->
+      cat == category && Map.has_key?(cat_tokens_map, token)
+    end)
+    |> Enum.count()
+  end
+
+  defp cat_trainings_count(category, data) do
+    data.categories[category][:trainings]
+  end
+
+  defp cat_base_count(category, data) do
+    cat_trainings_count(category, data) + 2
   end
 end
