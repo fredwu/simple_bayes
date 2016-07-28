467fceb80050e14b117d21fbd8e7a56554076de4
Fred Wu <ifredwu@gmail.com>
2016-07-28 21:00:02 +1000

    Doesn't feel the refactor made it better, so revert...
    Revert "Refactored `SimpleBayes.Classifier.Model.Bernoulli`"
    
    This reverts commit d3d1f533f530f3cc8a9e5d85a7cf4b280ba6c750.

--- lib/simple_bayes/classifier/models/bernoulli.ex
+++ lib/simple_bayes/classifier/models/bernoulli.ex
@@ -1,46 +1,29 @@
 defmodule SimpleBayes.Classifier.Model.Bernoulli do
   def probability_of(category, categories_map, data) do
-    numerator   = cat_trainings_count(category, data)
-                  * token_count(category, categories_map, data)
+    cat_trainings    = data.categories[category][:trainings]
+    denominator_base = cat_trainings + 2
 
-    denominator = data.trainings
-                  * :math.pow(
-                    cat_base_count(category, data),
-                    Kernel.map_size(data.tokens)
-                  )
+    numerator   = cat_trainings * category_numerator(category, denominator_base, categories_map, data)
+    denominator = data.trainings * :math.pow(denominator_base, Kernel.map_size(data.tokens))
 
     numerator / denominator
   end
 
-  defp token_count(category, categories_map, data) do
+  defp category_numerator(category, denominator_base, categories_map, data) do
     Enum.reduce(data.tokens, 1, fn ({token, _}, acc) ->
-      count = trainings_with_token_count(
-        data.tokens_per_training, category, token
-      ) + 1
+      token_in_trainings = data.tokens_per_training
+        |> Enum.filter(fn ({{cat, cat_tokens_map}, _}) ->
+          cat == category && Map.has_key?(cat_tokens_map, token)
+        end)
+        |> Enum.count()
 
       value = if Map.has_key?(categories_map, token) do
-        count
+        (token_in_trainings + 1)
       else
-        cat_base_count(category, data) - count
+        (denominator_base - (token_in_trainings + 1))
       end
 
       value * acc
     end)
-  end
-
-  defp trainings_with_token_count(tokens_map, category, token) do
-    tokens_map
-    |> Enum.filter(fn ({{cat, cat_tokens_map}, _}) ->
-      cat == category && Map.has_key?(cat_tokens_map, token)
-    end)
-    |> Enum.count()
-  end
-
-  defp cat_trainings_count(category, data) do
-    data.categories[category][:trainings]
-  end
-
-  defp cat_base_count(category, data) do
-    cat_trainings_count(category, data) + 2
   end
 end
